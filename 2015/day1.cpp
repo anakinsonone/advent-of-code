1b84a2db8d3d27de64e615e5b1e9bd470b70e8ee
Akhilesh Sonone <a.sonone@smartshiphub.com>
2025-07-03 23:59:55 +0530

    solution for day1 part2

--- 2015/day1.cpp
+++ 2015/day1.cpp
@@ -29,6 +29,17 @@ For example:
 ()) and ))( both result in floor -1 (the first basement level).
 ))) and )())()) both result in floor -3.
 To what floor do the instructions take Santa?
+--- Part Two ---
+Now, given the same instructions, find the position of the first character that
+causes him to enter the basement (floor -1). The first character in the
+instructions has position 1, the second character has position 2, and so on.
+
+For example:
+
+) causes him to enter the basement at character position 1.
+()()) causes him to enter the basement at character position 5.
+What is the position of the character that causes Santa to first enter the
+basement?
  */
 #include <iostream>
 #include <string>
@@ -47,7 +58,20 @@ int main() {
     }
   }
 
-  std::cout << floor;
+  std::cout << floor << '\n';
+
+  // part 2
+  int i = 0, count = 0;
+  while (count != -1) {
+    if (input[i] == '(') {
+      count++;
+    } else {
+      count--;
+    }
+    i++;
+  }
+
+  std::cout << i;
 
   return 0;
 }
